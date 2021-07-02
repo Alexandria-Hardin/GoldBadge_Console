c21937e5e9274937bf7e78a6742194283baeddd7
Alexandria Hardin <aphardin101@gmail.com>
2021-07-02 08:32:41 -0500

    Add project files.

---
+++ 01_CafeTest/CafeTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _01_CafeTest
+{
+    [TestClass]
+    public class CafeTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
